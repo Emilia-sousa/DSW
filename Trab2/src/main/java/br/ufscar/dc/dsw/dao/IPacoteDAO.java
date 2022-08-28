524141bcad631bc54540b8c6e9223d0a3e1a8e76
kevinviniciusjp <42143392+kevinviniciusjp@users.noreply.github.com>
2022-08-28 18:11:56 -0300

    Update IPacoteDAO.java

--- Trab2/src/main/java/br/ufscar/dc/dsw/dao/IPacoteDAO.java
+++ Trab2/src/main/java/br/ufscar/dc/dsw/dao/IPacoteDAO.java
@@ -3,8 +3,11 @@ package br.ufscar.dc.dsw.dao;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import br.ufscar.dc.dsw.domain.PacoteTuristico;
+import br.ufscar.dc.dsw.domain.Agencia;
 
 @SuppressWarnings("unchecked")
 public interface IPacoteDAO extends CrudRepository<PacoteTuristico, Long>{
@@ -16,4 +19,7 @@ public interface IPacoteDAO extends CrudRepository<PacoteTuristico, Long>{
 	PacoteTuristico save(PacoteTuristico pacote);
 
 	void deleteById(Long id);
+
+	@Query(value = "select * from PacoteTuristico p where p.destino like %:keyword% or p.dataPartida like %:keyword%", nativeQuery = true)
+	List<PacoteTuristico> findByKeyword(@Param("keyword") String keyword);
 }
